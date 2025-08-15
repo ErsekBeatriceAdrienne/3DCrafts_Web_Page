0eda52b70f7af73d458c71872c7c2c64eb80eed8
Bea <99785877+ErsekBeatriceAdrienne@users.noreply.github.com>
2025-08-15 21:01:17 +0300

    navigation bar in progress

--- 3dcrafts-website/src/App.js
+++ 3dcrafts-website/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ padding: "10px", background: "#333" }}>
+        <Link to="/" style={{ color: "#fff", marginRight: "15px" }}>Home</Link>
+        <Link to="/about" style={{ color: "#fff", marginRight: "15px" }}>About</Link>
+        <Link to="/contact" style={{ color: "#fff" }}>Contact</Link>
+      </nav>
+
+      <div style={{ padding: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
